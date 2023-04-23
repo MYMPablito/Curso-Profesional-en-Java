f02e783638c19671f94c8b1db9a2757bbeda0b69
MYMPablito <pablo-mato@hotmail.com>
2023-04-23 14:50:42 -0300

    Planteo y resolución ejercicio1 sobre tipos de datos

---
+++ PrimerosPasos/src/Ejercicio_1.java
@@ -0,0 +1,38 @@
+/*   ###Ejercicio.Declaración de variables ###
+ * 
+ * Crea una clase con el nombre de Ejercicio_1.Declara e inicia las variables que aparecen aquí:
+ * 
+ * nombre de la variable ---- tipo de dato ---- valor de la variable
+ *       nombre                 texto                   Facu
+ *        edad				  entero corto              30
+ *      salario				  decimal corto				1900.8				
+ *       carnet				     lógico                 false
+ * 
+ * 
+ * 
+ * Al ejecutar el programa debe aparecer el valor almacenado en cada variable uno debajo de otro en la consola de Eclipse */
+
+
+public class Ejercicio_1 {
+	
+	public static void main(String[] args) {
+		
+		String nombre = "Facu";
+		
+		byte edad = 30;
+		
+		double salario = 1900.8;
+		
+		boolean carnet = false;
+		
+		System.out.println(nombre);
+		
+		System.out.println(edad);
+		
+		System.out.println(salario);
+		
+		System.out.println(carnet);
+		
+	}
+
+}
