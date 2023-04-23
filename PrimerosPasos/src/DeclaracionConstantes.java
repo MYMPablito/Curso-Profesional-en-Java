5b7a08da9df10ec3d2479aa8f412e0df50ad8025
MYMPablito <pablo-mato@hotmail.com>
2023-04-23 15:45:52 -0300

    Ejemplo declaración constante, teoría operadores y ejemplo básico

---
+++ PrimerosPasos/src/DeclaracionConstantes.java
@@ -0,0 +1,31 @@
+
+public class DeclaracionConstantes {
+	
+	public static void main(String[] args) {
+		
+		final double CONVERSION_PESO_ARG = 450;
+		
+		System.out.println(CONVERSION_PESO_ARG);
+		
+		
+		
+		
+	}
+
+}
+
+/*  ############### OPERADORES EN JAVA ############## 
+ * 
+ * -Aritméticos: suma (+), resta(-), multiplicación(*) y división(/)
+ * 
+ * -Lógicos, relacionales y booleanos: mayor que(>), menor que(<), mayor o menor que(<>), mayor o igual que (>=), menor
+ * o igual que(<=), igualdad (==), distinto (!=), y lógico (&&), o lógico (||). -Se usan mucho a la hora de evaluar condiciones.
+ * 
+ * -Incremento y decremento: ++(incremento), --(decremento). Además: +=n°: incremento, -=n°: decremento.
+ * 
+ * 
+ * -Concatenación: +: une o concatena. La diferencia al aplicarlo siempre radica en el contexto.
+ *
+ * 
+ * */
+
---
+++ PrimerosPasos/src/UsoOperadores.java
@@ -0,0 +1,18 @@
+
+public class UsoOperadores {
+	
+	public static void main(String[] args) {
+		
+		int num1 = 8;
+		
+		int num2 = 6;
+		
+		num2 += 6;
+		
+		int resultado = num1 + num2;
+		
+		System.out.println(resultado);
+		
+	}
+
+}
